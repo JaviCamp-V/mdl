c45880b539ce897e8878b3a8ed0c53313f7c1192
Campbell, Javian D <campbelljd@jncb.com>
2024-08-01 15:27:25 -0500

    Allow server actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        'https://nextjs-14-0-2-bug-report-4ea064a57303.herokuapp.com',
+        'https://mdl-production-035f.up.railway.app',
+        'localhost:3000'
+      ]
+    }
+  },
   images: {
     remotePatterns: [
       {
